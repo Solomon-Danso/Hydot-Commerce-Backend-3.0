37c04f0350d58babcf53d3c04530c0760280799d
Solomon <solomondanso2023@gmail.com>
2024-10-10 23:10:24 +0000

    Setup

---
+++ src/test/java/com/hydottech/Hydot_commerce_30/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydottech.Hydot_commerce_30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
